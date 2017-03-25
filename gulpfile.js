cbe94767c74e444253b84ada1b5093aab78ef971
gjlacerda <guilherme.lcd@gmail.com>
2017-03-25 11:52:16 -0300

    Html e css geral

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,18 @@
-const gulp         = require('gulp');
-const less         = require('gulp-less');
-const concat       = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const tslint       = require('gulp-tslint');
-const gutil        = require('gulp-util');
-const fs           = require('fs');
+const gulp   = require('gulp');
+const tslint = require('gulp-tslint');
+const watch  = require('gulp-watch');
 
 let config = {
-    less: {
-        path: './src/assets/less/',
-        dest: './dist/assets/css/',
-        watch: [
-            './src/assets/less/**/*.less'
-        ]
-    },
     ts: {
         path: 'src/**/*.ts'
     }
 };
 
 /**
- * Compila os arquivos less
- */
-gulp.task('less', () => {
-
-    // Gera um arquivo CSS por módulo
-    fs.readdirSync(config.less.path).map((folder) => {
-
-        let path = config.less.path + folder + '/' + folder + '.less',
-            dest = config.less.dest + folder;
-
-        return gulp.src(path)
-                   .pipe(less().on('error', function(err) {
-                       gutil.log(err);
-                       this.emit('end');
-                   }))
-                   .pipe(autoprefixer({
-                       browsers: ['last 2 versions'],
-                       cascade: false
-                   }))
-                   .pipe(concat(folder + '.css'))
-                   .pipe(gulp.dest(dest));
-    });
-});
-
-/**
  * Validação (lint) de typescript
  */
 gulp.task('tslint', () => {
-    return gulp.src(config.typescript.path)
+    return gulp.src(config.ts.path)
                .pipe(tslint({
                    formatter: 'prose'
                }))
@@ -56,23 +20,9 @@ gulp.task('tslint', () => {
 });
 
 /**
- * Move as imagens
- */
-gulp.task('move:images', function() {
-
-    // Move imagens
-    gulp.src('./src/assets/images/*')
-        .pipe(gulp.dest('./dist/assets/images'));
-
-});
-
-/**
  * Watch
  */
 gulp.task('watch', () => {
-
-    // Less
-    gulp.watch(config.less.watch, ['less']);
 
     // Lint
     gulp.watch(config.ts.watch, ['tslint']);
@@ -81,4 +31,4 @@ gulp.task('watch', () => {
 /**
  * Default
  */
-gulp.task('default', ['less', 'watch', 'move:images']);
+gulp.task('default', ['tslint', 'watch']);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { VideoListComponent } from './video-list/video-list.component';
-import { PlayerComponent } from './player/player.component';
+import {AppComponent} from './app.component';
+import {VideoListComponent} from './video-list/video-list.component';
+import {PlayerComponent} from './player/player.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    VideoListComponent,
-    PlayerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        VideoListComponent,
+        PlayerComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
